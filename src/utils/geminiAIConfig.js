b3cde7c5b07b6a33e459180da5202d38c519a872
Mansi Patil <mp.mansipatil2000@gmail.com>
2024-11-14 11:32:04 +0530

    gemini ai key change

--- src/utils/geminiAIConfig.js
+++ src/utils/geminiAIConfig.js
@@ -1,5 +1,6 @@
+import {GEMINIAI_KEY} from "./constants"
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-export const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINIAI_KEY);
+export const genAI = new GoogleGenerativeAI(GEMINIAI_KEY);
 
 export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
